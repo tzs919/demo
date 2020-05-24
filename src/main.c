629d306795d8f4f82cefe3c536e0deea85f96c0b
tzs919 <tzs919@163.com>
2020-05-24 09:17:33 +0800

    增加CTest

--- src/main.c
+++ src/main.c
@@ -1,15 +1,25 @@
 #include "demo.h"
 
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char **argv)
 {
-    int x = 15;
-    int y = 16;
-    if (x <= 0 || y <= 0)
+    if (argc < 3)
     {
+        printf("Usage: %s x y \n", argv[0]);
         return 1;
     }
+
+    int x = atoi(argv[1]);
+    int y = atoi(argv[2]);
+
+    //演示断言assertion
+    if (x <= 0 || y <= 0)
+    {
+        return 2;
+    }
+
     int result = add(x, y);
     printf("%d + %d = %d\n", x, y, result);
 }
--- tests/test.cc
+++ tests/test.cc
@@ -2,7 +2,12 @@
 
 #include "demo.h"
 
-TEST(DEMO, should_return_integer)
+TEST(DEMO, should_return_integer3)
 {
     EXPECT_EQ(add(1, 2), 3);
+}
+
+TEST(DEMO, should_return_integer50)
+{
+    EXPECT_EQ(add(23, 27), 50);
 }
